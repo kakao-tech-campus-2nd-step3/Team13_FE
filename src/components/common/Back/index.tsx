699b2c54db504e9d88fe2d13883e481987739b26
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-09-29 16:50:49 +0900

    feat: 여러 컴포넌트 추가

---
+++ src/components/common/Back/index.tsx
@@ -0,0 +1,20 @@
+import backArrow from '@/assets/icons/back_arrow.svg'
+
+function Back() {
+  return (
+    <div
+      onClick={() => {}}
+      style={{
+        width: '100vw',
+        paddingLeft: '15px',
+        boxSizing: 'border-box',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <img src={backArrow} alt="back_arrow" />
+    </div>
+  )
+}
+
+export default Back
