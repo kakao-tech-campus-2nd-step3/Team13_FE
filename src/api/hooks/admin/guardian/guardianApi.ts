84db199c2117bd3924c946f0bf761b8145ad1e74
Jisu Lee <dlwltn0430@naver.com>
2024-11-07 01:54:33 +0900

    fix: 보호자 등록 request body 수정

--- src/api/hooks/admin/guardian/guardianApi.ts
+++ src/api/hooks/admin/guardian/guardianApi.ts
@@ -5,18 +5,16 @@ const GUARDIAN_BASE_URL = '/v1/institution/guardian'
 
 export const addGuardian = async (newGuardian: Partial<Guardian>) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // const { id, isNew, ...guardianData } = newGuardian
-  return await fetchInstance.post(GUARDIAN_BASE_URL, newGuardian)
+  const { id, isNew, isActive, ...guardianData } = newGuardian
+  return await fetchInstance.post(GUARDIAN_BASE_URL, guardianData)
 }
 
 export const getGuardians = async (): Promise<Guardian[]> => {
   const response = await fetchInstance.get<GuardianResponseData>(GUARDIAN_BASE_URL)
-  return response.data.recipients
+  return response.data.guardians
 }
 
 export const updateGuardian = async (id: number, updatedData: Partial<Guardian>) => {
-  // const updatedInstitution = { institutionNumber: id, ...updatedData }
-  // console.log(updatedInstitution)
   return await fetchInstance.put(`${GUARDIAN_BASE_URL}/${id}`, updatedData)
 }
 
