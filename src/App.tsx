868d46fca3f1ccc4ba1ebaf04f2100b21c765cd2
Jisu Lee <dlwltn0430@naver.com>
2024-11-09 20:36:46 +0900

    chore: chakra-ui 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import '@/App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Routes } from './routes/routes'
-import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 
 const queryClient = new QueryClient()
-
-// function App() {
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <div className="App">
-//         <BrowserRouter><Routes /></BrowserRouter>
-//       </div>
-//     </QueryClientProvider>
-//   )
-// }
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Routes />
-      </div>
+      <ChakraProvider value={defaultSystem}>
+        <div className="App">
+          <Routes />
+        </div>
+      </ChakraProvider>
     </QueryClientProvider>
   )
 }
