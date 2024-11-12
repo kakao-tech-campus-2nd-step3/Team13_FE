06193cba5240dcbb5f3d6ed86f509a62c7d296ab
Jisu Lee <dlwltn0430@naver.com>
2024-11-13 03:55:53 +0900

    fix: 돌봄대상자 API 요청 로직 수정

--- src/api/hooks/admin/recipient/recipientApi.ts
+++ src/api/hooks/admin/recipient/recipientApi.ts
@@ -1,25 +1,25 @@
-import { fetchInstance } from '@/api/instance/instance'
+import apiInstance from '@/provider/Auth/apiInstance'
 import type { Recipient, RecipientResponseData } from './types'
 
-const RECIPIENT_BASE_URL = '/v1/recipient'
+const RECIPIENT_BASE_URL = '/v1/admin/recipient'
 
 export const addRecipient = async (newRecipient: Partial<Recipient>) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { id, isNew, ...recipientData } = newRecipient
-  return await fetchInstance.post(RECIPIENT_BASE_URL, recipientData)
+  return await apiInstance.post(RECIPIENT_BASE_URL, recipientData)
 }
 
 export const getRecipients = async (): Promise<Recipient[]> => {
-  const response = await fetchInstance.get<RecipientResponseData>(RECIPIENT_BASE_URL)
-  return response.data.recipients
+  const response = await apiInstance.get<RecipientResponseData>(RECIPIENT_BASE_URL)
+  return response.data.response
 }
 
 export const updateRecipient = async (id: number, updatedData: Partial<Recipient>) => {
   // const updatedInstitution = { institutionNumber: id, ...updatedData }
   // console.log(updatedInstitution)
-  return await fetchInstance.put(`${RECIPIENT_BASE_URL}/${id}`, updatedData)
+  return await apiInstance.put(`${RECIPIENT_BASE_URL}/${id}`, updatedData)
 }
 
 export const deleteRecipient = async (id: number) => {
-  return await fetchInstance.delete(`${RECIPIENT_BASE_URL}/${id}`)
+  return await apiInstance.delete(`${RECIPIENT_BASE_URL}/${id}`)
 }
