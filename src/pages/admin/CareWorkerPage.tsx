637131b4817e1cb673d303fbbd1c738dee749ae5
Jisu Lee <dlwltn0430@naver.com>
2024-11-13 04:48:37 +0900

    style: mock 데이터 삭제

--- src/pages/admin/CareWorkerPage.tsx
+++ src/pages/admin/CareWorkerPage.tsx
@@ -5,11 +5,6 @@ import { useGetCareWorkers } from '@/api/hooks/admin/careWorker/useGetCareWorker
 import { useUpdateCareWorker } from '@/api/hooks/admin/careWorker/useUpdateCareWorker'
 import { Table } from '@/components/common/Table/Table'
 
-// const careWorkers: CareWorker[] = [
-//   { id: 1, institutionId: 100, name: '이지수', email: '1234@example.com', phone: '010-0000-0000' },
-//   { id: 2, institutionId: 101, name: '이지수', email: '1234@example.com', phone: '010-0000-0000' },
-// ]
-
 const columns: { key: keyof CareWorker; label: string }[] = [
   { key: 'id', label: 'ID' },
   { key: 'institutionId', label: '요양원 ID' },
