c192c0aba1756f19b4787568cf566f0bc1621c3b
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-11-12 22:09:26 +0900

    fix: 에러 해결

--- src/api/hooks/user/chart/usePostChart.ts
+++ src/api/hooks/user/chart/usePostChart.ts
@@ -29,51 +29,30 @@ export const submitChartData = async () => {
       mealType: bodyManagement.mealType,
       intakeAmount: bodyManagement.intakeAmount,
       physicalRestroom: bodyManagement.physicalRestroom,
-<<<<<<<< HEAD:src/api/hooks/chart/usePostChart.tsx
-      has_walked: bodyManagement.has_walked,
-      isPositionChangeRequired: bodyManagement.isPositionChangeRequired,
-      isMobilityAssistance: bodyManagement.isMobilityAssistance,
-========
       hasWalked: bodyManagement.hasWalked,
       positionChangeRequired: bodyManagement.positionChangeRequired,
       mobilityAssistance: bodyManagement.mobilityAssistance,
->>>>>>>> week11:src/api/hooks/user/chart/usePostChart.ts
       physicalNote: localStorage.getItem('physicalNote') || '',
     },
     cognitiveManagement: {
       cognitiveHelp: cognitiveManagement.cognitiveHelp,
-<<<<<<<< HEAD:src/api/hooks/chart/usePostChart.tsx
-      isCompanionshipProvided: cognitiveManagement.isCompanionshipProvided,
-========
       companionshipProvided: cognitiveManagement.companionshipProvided,
->>>>>>>> week11:src/api/hooks/user/chart/usePostChart.ts
       cognitiveNote: localStorage.getItem('cognitiveNote') || '',
     },
     nursingManagement: {
       systolic: nursingManagement.systolic,
       diastolic: nursingManagement.diastolic,
       healthTemperature: nursingManagement.healthTemperature,
-<<<<<<<< HEAD:src/api/hooks/chart/usePostChart.tsx
-      isHealthCareProvided: nursingManagement.isHealthCareProvided,
-      isNursingCareProvided: nursingManagement.isNursingCareProvided,
-      isEmergencyCareProvided: nursingManagement.isEmergencyCareProvided,
-========
       healthCareProvided: nursingManagement.healthCareProvided,
       nursingCareProvided: nursingManagement.nursingCareProvided,
       emergencyCareProvided: nursingManagement.emergencyCareProvided,
->>>>>>>> week11:src/api/hooks/user/chart/usePostChart.ts
       healthNote: localStorage.getItem('healthNote') || '',
     },
     recoveryTraining: {
       recoveryProgram: recoveryTraining.recoveryProgram,
       recoveryTraining: recoveryTraining.recoveryTraining,
-<<<<<<<< HEAD:src/api/hooks/chart/usePostChart.tsx
-      isCognitiveTrainingProvided: recoveryTraining.isCognitiveTrainingProvided,
-      isPhysicalTherapyProvided: recoveryTraining.isPhysicalTherapyProvided,
-========
       cognitiveTrainingProvided: recoveryTraining.cognitiveTrainingProvided,
       physicalTherapyProvided: recoveryTraining.physicalTherapyProvided,
->>>>>>>> week11:src/api/hooks/user/chart/usePostChart.ts
       recoveryNote: localStorage.getItem('recoveryNote') || '',
     },
   }
@@ -81,16 +60,6 @@ export const submitChartData = async () => {
   try {
     await apiInstance.post(postChartPath(), chartData)
     console.log('Chart data submitted successfully!')
-    localStorage.removeItem('bodyManagement')
-    localStorage.removeItem('cognitiveManagement')
-    localStorage.removeItem('nursingManagement')
-    localStorage.removeItem('recoveryTraining')
-    localStorage.removeItem('conditionDisease')
-    localStorage.removeItem('recipientId')
-    localStorage.removeItem('physicalNote')
-    localStorage.removeItem('cognitiveNote')
-    localStorage.removeItem('healthNote')
-    localStorage.removeItem('recoveryNote')
   } catch (error) {
     console.error('Error submitting chart data:', error)
   }
