1f1c7ace3dd6fff2fcae011e17840b38c6733aeb
Jisu Lee <dlwltn0430@naver.com>
2024-11-14 04:36:26 +0900

    design: 음성녹음 UI 수정

--- src/pages/AudioRecord/AudioRecord.tsx
+++ src/pages/AudioRecord/AudioRecord.tsx
@@ -2,62 +2,21 @@ import Steps from '@/components/common/Steps/Steps'
 import { Heading, Paragraph, TextBody } from '@/components/common/Text/TextFactory'
 import { colors } from '@/styles/colors/colors'
 import { BeatLoader } from 'react-spinners'
-import { useRef, useState } from 'react'
-import play from '@/assets/icons/play.svg'
+import { useState } from 'react'
 import styled from 'styled-components'
+import { useSpeechToText } from '@/components/features/SpeechToText/hooks/useSpeechToText'
+import { FaPlay, FaStop } from 'react-icons/fa'
 
 export const AudioRecordPage = () => {
   const [isRecording, setIsRecording] = useState(false)
-  const [audioUrl, setAudioUrl] = useState<string | null>(null)
-  const audioChunks = useRef<Blob[]>([])
-  const mediaRecorder = useRef<MediaRecorder | null>(null)
 
-  const startRecording = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
-      const recorder = new MediaRecorder(stream, { mimeType: 'audio/webm;codecs=opus' })
-      mediaRecorder.current = recorder
-
-      recorder.ondataavailable = (event) => {
-        audioChunks.current.push(event.data)
-      }
-
-      recorder.onstop = () => {
-        if (audioUrl) {
-          URL.revokeObjectURL(audioUrl)
-        }
-        const audioBlob = new Blob(audioChunks.current, { type: 'audio/webm' })
-        const newAudioUrl = `${URL.createObjectURL(audioBlob)}#${Date.now()}`
-        setAudioUrl(newAudioUrl)
-        downloadAudioFile(audioBlob)
-
-        audioChunks.current = []
-      }
-
-      recorder.start()
-      setIsRecording(true)
-    } catch (error) {
-      console.error('Error accessing microphone:', error)
-      alert('마이크에 접근할 수 없습니다. 권한을 허용해 주세요.')
-    }
+  const handleToggleListening = () => {
+    toggleListening()
+    setIsRecording(!listening)
   }
 
-  const stopRecording = () => {
-    if (mediaRecorder) {
-      mediaRecorder.current?.stop()
-      setIsRecording(false)
-    }
-  }
-
-  const downloadAudioFile = (audioBlob: Blob) => {
-    const link = document.createElement('a')
-    const url = URL.createObjectURL(audioBlob)
-    const fileName = `recording_${Date.now().toString()}.webm`
-    link.href = url
-    link.download = fileName
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
+  const handleResetAndListen = () => {
+    setIsRecording(true)
   }
 
   return (
@@ -82,70 +41,25 @@ export const AudioRecordPage = () => {
         <Paragraph.Large style={{ color: colors.text.subtle }}>
           주변 소음이 들리지 않도록 해주세요.
         </Paragraph.Large>
-        <div>
-          {isRecording ? (
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'start',
-                alignItems: 'center',
-                height: '210px',
-              }}
-            >
-              <RecordCircle>
-                <BeatLoader
-                  color="#4894FE"
-                  margin={6}
-                  size={8}
-                  speedMultiplier={1.2}
-                  style={{ height: '80px', width: '80px' }}
-                />
-              </RecordCircle>
-              <Paragraph.Large style={{ color: colors.text.subtle }}>00:05:36</Paragraph.Large>
-            </div>
-          ) : (
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'start',
-                alignItems: 'center',
-                height: '210px',
-              }}
-            >
-              <RecordCircle>
-                <BeatLoader
-                  color="#4894FE"
-                  margin={6}
-                  size={8}
-                  speedMultiplier={0}
-                  style={{ height: '80px', width: '80px' }}
-                />
-              </RecordCircle>
-            </div>
-          )}
-        </div>
-      </div>
-      <div>
-        {isRecording ? (
-          <button onClick={stopRecording}>녹음 중지</button>
-        ) : (
-          <button onClick={startRecording}>녹음 시작</button>
-        )}
-
-        {audioUrl && (
-          <div>
-            <h2>녹음된 파일:</h2>
-            <audio controls preload="auto" src={audioUrl}></audio>
-          </div>
-        )}
-      </div>
+        <RecordSection>
+          <RecordCircle>
+            <BeatLoader
+              color={colors.primary.main}
+              margin={6}
+              size={8}
+              speedMultiplier={isRecording ? 1.2 : 0}
+            />
+          </RecordCircle>
+        </RecordSection>
+      </Content>
+      <TextArea value={transcript} readOnly />
       <WaveBackground>
         <Mountain3 />
         <Mountain2 />
         <Mountain1 />
-        <CenteredImage src={play} alt={play} />
+        <CenteredImage onClick={isRecording ? handleToggleListening : handleResetAndListen}>
+          {isRecording ? <FaStop color="#fff" size="30" /> : <FaPlay color="#fff" size="30" />}
+        </CenteredImage>
       </WaveBackground>
     </Wrapper>
   )
@@ -156,7 +70,22 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  padding: 0 23px;
+  padding: 30px 24px 0 24px;
+`
+
+const Content = styled.div`
+  //flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const RecordSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 210px;
 `
 
 const RecordCircle = styled.div`
@@ -169,6 +98,18 @@ const RecordCircle = styled.div`
   text-align: center;
   margin-top: 80px;
   margin-bottom: 20px;
+`
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 100px;
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid ${colors.border.subtle};
+  font-size: 1rem;
+  color: ${colors.border.prominent};
+  resize: none;
+  outline: none;
 `
 
 const WaveBackground = styled.div`
@@ -205,12 +146,11 @@ const Mountain3 = styled.div`
   background-color: #e4efff;
   clip-path: ellipse(100% 90% at 30% 100%);
 `
-const CenteredImage = styled.img`
+
+const CenteredImage = styled.div`
   position: absolute;
   bottom: 70px;
   left: 50%;
   transform: translateX(-50%);
   z-index: 4;
-  width: 34px;
-  height: 40px;
 `
