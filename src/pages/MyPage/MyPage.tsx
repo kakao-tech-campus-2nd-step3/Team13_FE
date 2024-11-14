f88fdbfb894616ecde107d06e0546cd5f910067a
Jisu Lee <dlwltn0430@naver.com>
2024-11-14 10:43:51 +0900

    feat: 마이페이지 조회 API 연결

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -3,41 +3,33 @@ import * as S from './MyPage.styles'
 import { IoPersonCircle } from 'react-icons/io5'
 import Button from '@/components/common/Button/Button'
 import { Input } from '@chakra-ui/react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useLogout } from '@/api/hooks/common/useLogout'
+import { useUserInfo } from '@/api/hooks/user/my/useUserInfo'
 
-// 추후 삭제 예정
-const mockData = {
-  name: '김쿠키님',
-  nursingHome: '00 요양원',
-  workDays: '월, 수, 금',
-  alarmTime: '08:00',
-}
+export const MyPage = () => {
+  const { data, isLoading, isError } = useUserInfo()
 
-export const MyPageWithMock = () => <MyPage {...mockData} />
+  const logout = useLogout()
 
-export default MyPageWithMock
+  const [alarmTime, setAlarmTime] = useState(data?.alertTime || '')
+  const [smsSubscription, setSmsSubscription] = useState(data?.smsSubscription || false)
+  const [lineSubscription, setLineSubscription] = useState(data?.lineSubscription || false)
 
-interface MyPageProps {
-  name: string
-  nursingHome: string
-  workDays: string
-  alarmTime: string
-}
-
-export const MyPage: React.FC<MyPageProps> = ({
-  name,
-  nursingHome,
-  workDays,
-  alarmTime: propsAlarmTime,
-}) => {
-  const [alarmTime, setAlarmTime] = useState(propsAlarmTime)
+  useEffect(() => {
+    if (data) {
+      setAlarmTime(data.alertTime || '')
+      setSmsSubscription(data.smsSubscription || false)
+      setLineSubscription(data.lineSubscription || false)
+    }
+  }, [data])
 
   const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setAlarmTime(e.target.value)
   }
 
-  const logout = useLogout()
+  if (isLoading) return <p>Loading...</p>
+  if (isError) return <p>Error loading user data.</p>
 
   return (
     <S.Container>
@@ -46,31 +38,67 @@ export const MyPage: React.FC<MyPageProps> = ({
         <div>
           <IoPersonCircle size="100" color={colors.border.subtle} />
         </div>
-        <S.ProfileName>{name}</S.ProfileName>
+        <S.ProfileName>{data?.name || ''}</S.ProfileName>
         <S.LogOut onClick={logout}>로그아웃</S.LogOut>
       </S.ProfileSection>
       <S.InfoSection>
         <S.InfoItem>
           <S.Label>역할</S.Label>
-          <S.Value>요양보호사</S.Value>
+          <S.Value>
+            {localStorage.getItem('role') === 'careworker' ? '요양보호사' : '보호자'}
+          </S.Value>
         </S.InfoItem>
+        {localStorage.getItem('role') === 'careworker' && data && (
+          <>
+            <S.InfoItem>
+              <S.Label>소속</S.Label>
+              <S.Value>{'institutionName' in data ? data.institutionName : ''}</S.Value>
+            </S.InfoItem>
+            <S.InfoItem>
+              <S.Label>근무일</S.Label>
+              <S.Value>{'workingDays' in data ? data.workingDays.join(', ') : ''}</S.Value>
+            </S.InfoItem>
+          </>
+        )}
         <S.InfoItem>
-          <S.Label>소속</S.Label>
-          <S.Value>{nursingHome}</S.Value>
-        </S.InfoItem>
-        <S.InfoItem>
-          <S.Label>근무일</S.Label>
-          <S.Value>{workDays}</S.Value>
+          <S.Label>연락처</S.Label>
+          <S.Value>{data?.phone || ''}</S.Value>
         </S.InfoItem>
         <S.InfoItem>
           <S.Label>알림 시간</S.Label>
           <S.Value>
-            <Input type="time" defaultValue={alarmTime} onChange={handleTimeChange} />
+            <Input type="time" value={alarmTime} onChange={handleTimeChange} />
+          </S.Value>
+        </S.InfoItem>
+        <S.InfoItem>
+          <S.Label>SMS 수신 동의</S.Label>
+          <S.Value>
+            <label>
+              <S.Checkbox
+                type="checkbox"
+                checked={smsSubscription}
+                onChange={() => setSmsSubscription(!smsSubscription)}
+              />
+              동의
+            </label>
+          </S.Value>
+        </S.InfoItem>
+        <S.InfoItem>
+          <S.Label>LINE 수신 동의</S.Label>
+          <S.Value>
+            <label>
+              <S.Checkbox
+                type="checkbox"
+                checked={lineSubscription}
+                onChange={() => setLineSubscription(!lineSubscription)}
+              />
+              동의
+            </label>
           </S.Value>
         </S.InfoItem>
       </S.InfoSection>
       <Button theme="dark" width="300px" margin="40px">
-        확인
+        수정
       </Button>
     </S.Container>
   )
