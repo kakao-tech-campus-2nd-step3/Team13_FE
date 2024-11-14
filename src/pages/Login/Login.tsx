b77551d22ad100eec927c4c252e6214ef040f9ef
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-11-14 21:59:01 +0900

    feat: 로그인 페이지 수정

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -10,7 +10,8 @@ export const LoginPage = () => {
   const [userId, setId] = useState('')
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
-
+  const role = localStorage.getItem('role')
+  console.log(role)
   const login = async (event: React.FormEvent) => {
     event.preventDefault()
     const role = localStorage.getItem('role')
@@ -31,19 +32,36 @@ export const LoginPage = () => {
           <div style={{ fontSize: '24px', fontWeight: '700', marginBottom: '10px' }}>
             안녕하세요 :) 돌봄다리입니다.
           </div>
-          <div style={{ color: colors.primary.main, fontSize: '15px' }}>
-            전화번호와 비밀번호를 입력해주세요.
-          </div>
+          {role === 'careworker' || role === 'guardian' ? (
+            <div style={{ color: colors.primary.main, fontSize: '15px' }}>
+              전화번호와 비밀번호를 입력해주세요.
+            </div>
+          ) : (
+            <div style={{ color: colors.primary.main, fontSize: '15px' }}>
+              아이디와 비밀번호를 입력해주세요.
+            </div>
+          )}
         </div>
         <div>
-          <InputField
-            placeholder="전화번호 ( '-' 제외)"
-            id="userId"
-            name="userId"
-            value={userId}
-            onChange={(e) => setId(e.target.value)}
-            style={{ fontSize: '20px', marginBottom: '20px' }}
-          />
+          {role === 'careworker' || role === 'guardian' ? (
+            <InputField
+              placeholder="전화번호 ( '-' 제외)"
+              id="userId"
+              name="userId"
+              value={userId}
+              onChange={(e) => setId(e.target.value)}
+              style={{ fontSize: '20px', marginBottom: '20px' }}
+            />
+          ) : (
+            <InputField
+              placeholder="아이디"
+              id="userId"
+              name="userId"
+              value={userId}
+              onChange={(e) => setId(e.target.value)}
+              style={{ fontSize: '20px', marginBottom: '20px' }}
+            />
+          )}
           <InputField
             placeholder="비밀번호"
             id="password"
