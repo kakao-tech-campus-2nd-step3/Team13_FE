340032e672ac45397e2e90560c6378d55ec92cf4
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-11-14 21:35:11 +0900

    feat: 관리자, 요양원 로그아웃 버튼

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -27,7 +27,7 @@ export const LayoutWithoutMyPage = () => (
 export const AdminLayout = () => (
   <AdminWrapperBody>
     <AdminWrapper>
-      <Back myPage={false} />
+      <Back myPage={false} logout={true} />
       <InnerWrapper>
         <Outlet />
       </InnerWrapper>
@@ -72,7 +72,6 @@ const AdminWrapperBody = styled.div`
 const AdminWrapper = styled.div`
   width: 100%;
   min-height: 100vh;
-  padding-top: 15px;
   position: relative;
 `
 const InnerWrapper = styled.div`
