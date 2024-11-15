f5d38aef01f37c6eec32d473b57e1cd670ed5e41
Jisu Lee <dlwltn0430@naver.com>
2024-11-15 15:17:08 +0900

    design: OCR 인식 결과 테이블 디자인

--- src/pages/OCR/OCRTable/OCRTable.tsx
+++ src/pages/OCR/OCRTable/OCRTable.tsx
@@ -16,22 +16,22 @@ export const OCRTable: React.FC<{ data: Data }> = ({ data }) => {
   return (
     <S.Table>
       <thead>
-        <S.TableRow>
+        <tr>
           <S.TableHeader>항목명</S.TableHeader>
           <S.TableHeader>택1 또는 서술</S.TableHeader>
-        </S.TableRow>
+        </tr>
       </thead>
       <tbody>
         {Object.entries(data).map(([sectionName, sectionData]) => (
           <React.Fragment key={sectionName}>
-            <S.SectionTitleRow>
+            <tr>
               <S.SectionTitle colSpan={2}>{sectionName}</S.SectionTitle>
-            </S.SectionTitleRow>
+            </tr>
             {Object.entries(sectionData).map(([item, value]) => (
-              <S.TableRow key={item}>
+              <tr key={item}>
                 <S.TableData>{item}</S.TableData>
                 <S.TableData>{value as string}</S.TableData>
-              </S.TableRow>
+              </tr>
             ))}
           </React.Fragment>
         ))}
