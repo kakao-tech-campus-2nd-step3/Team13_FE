e776bdbc5d4131b1367142fe489e1e500fea0f81
Jisu Lee <dlwltn0430@naver.com>
2024-11-19 04:36:56 +0900

    feat: OCR로 차트 등록 시 스피너 동작하도록 구현

--- src/pages/OCR/OCRLoading/OCRLoading.tsx
+++ src/pages/OCR/OCRLoading/OCRLoading.tsx
@@ -2,7 +2,11 @@ import { Heading } from '@/components/common/Text/TextFactory'
 import { colors } from '@/styles/colors/colors'
 import styled, { keyframes } from 'styled-components'
 
-export const OCRLoadingPage = () => {
+interface OCRLoadingPageProps {
+  text?: string
+}
+
+export const OCRLoadingPage: React.FC<OCRLoadingPageProps> = ({ text }) => {
   return (
     <Wrapper>
       <div style={{ marginBottom: '37px', width: '130px', height: '130px' }}>
@@ -17,13 +21,18 @@ export const OCRLoadingPage = () => {
           textAlign: 'center',
         }}
       >
-        손글씨를 인식 중입니다
-        <br />
-        잠시만 기다려주세요
+        {text || (
+          <>
+            손글씨를 인식 중입니다
+            <br />
+            잠시만 기다려주세요
+          </>
+        )}
       </Heading.Medium>
     </Wrapper>
   )
 }
+
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
